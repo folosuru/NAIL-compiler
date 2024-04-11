00a53816909d635aaf8c2840416aed30cd5a8471
folosuru <folosuru@gmail.com>
2024-04-11 16:21:28 +0900

    add suggest

--- src/error/ErrorPrinter.cpp
+++ src/error/ErrorPrinter.cpp
@@ -6,16 +6,18 @@ namespace NAIL_cl {
     namespace ErrorPrinter {
         void print(const std::shared_ptr<TokenList>& list,
                    const std::shared_ptr<Token::Token>& token,
-                   const std::string& message) {
+                   const std::string& message,
+                   std::optional<std::string_view> suggest) {
             std::size_t line = token->line;
-            std::string target_line = list->source_text.at(line);
+            std::string target_line = list->source_text.at(line-1);
             std::cout << message << "\n";
 
-            std::cout << std::setw(4) << line+1 << ":" << token->pos+1
+            std::cout << std::setw(4) << line << ":" << token->pos+1
             << std::right  << " | " << target_line << "\n";
-            std::cout << std::setw( token->pos+1 + 4 + 4 + std::log10(token->pos+1)+1) << "^";
-            for (int i = 1; i < token->getString().size(); ++i) {
-                std::cout << "~";
+            std::cout << std::setw( token->pos+1 + 4 + 4 + std::log10(token->pos+1)+1) << "^"
+             <<  std::setfill('~') << std::setw(token->getString().size() - 1) << "" << std::setfill(' ') << "\n";
+            if (suggest) {
+                std::cout << std::setw( token->pos+1 + 4 + 4 + std::log10(token->pos+1)+1) << suggest.value() << "\n";
             }
             exit(0);
         }
--- src/error/ErrorPrinter.hpp
+++ src/error/ErrorPrinter.hpp
@@ -2,11 +2,15 @@
 #define NAIL_CL_ERRORPRINTER_HPP
 #include <tokenize/TokenList.hpp>
 #include <tokenize/token/Token.hpp>
+#include <optional>
 namespace NAIL_cl {
 
     namespace ErrorPrinter {
         [[noreturn]]
-        void print(const std::shared_ptr<TokenList>&, const std::shared_ptr<Token::Token>&, const std::string& message);
+        void print(const std::shared_ptr<TokenList>&,
+                   const std::shared_ptr<Token::Token>&,
+                   const std::string& message,
+                   std::optional<std::string_view> suggest = std::nullopt);
 
     };
 
